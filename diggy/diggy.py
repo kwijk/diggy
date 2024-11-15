8762de43b1eaf5de65f3546681aec30136aa7f76
kees van wijk <kees@mriguidance.com>
2024-11-15 19:38:00 +0100

    add basic cstore functionality

--- diggy/diggy.py
+++ diggy/diggy.py
@@ -1,6 +1,6 @@
 import logging
-
-from pynetdicom import AE, evt, _config, ALL_TRANSFER_SYNTAXES
+import pydicom
+from pynetdicom import AE, evt, _config, ALL_TRANSFER_SYNTAXES, AllStoragePresentationContexts
 from pynetdicom.sop_class import Verification
 from pynetdicom.events import Event
 
@@ -18,17 +18,24 @@ class Diggy:
         block: bool = True,
     ):
         self.ae = AE(ae_title=ae_title)
-        self.ae.add_supported_context(Verification)
+        self.ae.add_supported_context(Verification, ALL_TRANSFER_SYNTAXES)
+        for cx in AllStoragePresentationContexts:
+            self.ae.add_supported_context(
+                cx.abstract_syntax,
+                ALL_TRANSFER_SYNTAXES
+            )
         self.storage_folder = storage_folder
 
         self.logger = logging.getLogger("diggy")
 
-        handlers = [(evt.EVT_C_ECHO, self.handle_echo)]
+        handlers = [
+            (evt.EVT_C_ECHO, self.handle_echo),
+            (evt.EVT_C_STORE, self.handle_store),
+        ]
 
         self.ae.start_server(address, evt_handlers=handlers, block=block)
 
     def handle_echo(self, event: Event):
-        """Handle a C-ECHO request event."""
         ae = event.assoc.requestor.ae_title
         ip = event.assoc.requestor.address
         port = event.assoc.requestor.port
@@ -37,8 +44,19 @@ class Diggy:
 
         return 0x0000
 
+    def handle_store(self, event: Event):
+        ae = event.assoc.requestor.ae_title
+        ip = event.assoc.requestor.address
+        port = event.assoc.requestor.port
+
+        self.logger.info(f"C-Store request from {ae} at ({ip}:{port})")
+
+        return 0x0000
+
+
+
     def send_c_echo(self, aet: str, address: str, port: int):
-        ae = AE(ae_title=self.ae.ae_title)
+        ae = AE(ae_title=aet)
         ae.add_requested_context(Verification)
         assoc = ae.associate(address, port)
         if assoc.is_established:
@@ -52,6 +70,34 @@ class Diggy:
                 self.logger.info(
                     "Connection timed out, was aborted or received invalid response"
                 )
+
+            # Release the association
+            assoc.release()
+
+        else:
+            self.logger.info("Association rejected, aborted or never connected")
+
+    def send_c_store(self, aet: str, address: str, port: int, instances: list[pydicom.Dataset]):
+
+        ae = AE(ae_title=aet)
+
+        sop_classes = {str(ds.SOPClassUID) for ds in instances if 'SOPClassUID' in ds}
+        for sop_class_uid in sop_classes:
+            ae.add_requested_context(sop_class_uid)
+
+        assoc = ae.associate(address, port)
+        if assoc.is_established:
+            for ds in instances:
+                status = assoc.send_c_store(ds)
+                if status:
+                    # If the verification request succeeded this will be 0x0000
+                    self.logger.info(
+                        "C-Store request status: 0x{0:04x}".format(status.Status)
+                    )
+                else:
+                    self.logger.info(
+                        "Connection timed out, was aborted or received invalid response"
+                    )
 
             # Release the association
             assoc.release()
